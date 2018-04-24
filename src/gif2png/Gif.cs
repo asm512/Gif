32ad5f14ec5cd3c000cf27f60aa13cf3991d5403
peach <skpikao233@gmail.com>
2018-04-24 17:36:58 +0100

    Add refactored source.

---
+++ src/gif2png/Gif.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace gif2png
+{
+    class Gif
+    {
+        public string Path { private set; get; }
+
+        public string ExtractPath { private set; get; }
+
+        public Gif(string path)
+        {
+            if (!File.Exists(path)) { throw new FileNotFoundException(); }
+            this.Path = path;
+        }
+
+        public override string ToString() => System.IO.Path.GetFileName(Path);
+
+        public void SetExtractPath(string path)
+        {
+            if (!Directory.Exists(path)) { throw new DirectoryNotFoundException(); }
+            if (path.EndsWith(@"\")) { this.ExtractPath = path; }
+            else { this.ExtractPath = path + @"\"; }
+        }
+
+        private double ConvertByteToMegabyte(long bytes)
+        {
+            return (bytes / 1024f) / 1024f;
+        }
+
+        public double Size()
+        {
+            Int64 fileSizeInBytes = new FileInfo(Path).Length;
+            return Math.Round(ConvertByteToMegabyte(fileSizeInBytes), 3);
+        }
+
+        public int FrameInterval()
+        {
+            Image gif = Image.FromFile(Path);
+            PropertyItem item = gif.GetPropertyItem(0x5100);
+            int interval = (item.Value[0] + item.Value[1] * 256) * 13;
+            return interval;
+        }
+
+        public int FrameCount()
+        {
+            Image gif = Image.FromFile(Path);
+            FrameDimension dimension = new FrameDimension(gif.FrameDimensionsList[0]);
+            int frames = gif.GetFrameCount(dimension);
+            return frames;
+        }
+
+        public enum OutputFormat
+        {
+            JPG,
+            PNG,
+            BMP,
+            ICO
+        }
+
+        public void ExtractFrame(int frame, OutputFormat extractFormat, string prefix = "x")
+        {
+            Image gifImg = Image.FromFile(Path);
+            gifImg.SelectActiveFrame(FrameDimension.Time, frame);
+
+            switch (extractFormat)
+            {
+                case OutputFormat.JPG:
+                    var outputJpg = ExtractPath + prefix + frame + ".jpg";
+                    gifImg.Save(outputJpg, ImageFormat.Jpeg);
+                    return;
+
+                case OutputFormat.PNG:
+                    var outputPng = ExtractPath + prefix + frame + ".png";
+                    gifImg.Save(outputPng, ImageFormat.Png);
+                    return;
+
+                case OutputFormat.BMP:
+                    var outputBMP = ExtractPath + prefix + frame + ".bmp";
+                    gifImg.Save(outputBMP, ImageFormat.Bmp);
+                    return;
+
+                case OutputFormat.ICO:
+                    var outputIco = ExtractPath + prefix + frame + ".ico";
+                    gifImg.Save(outputIco, ImageFormat.Icon);
+                    return;
+            }
+        }
+
+        public void Extract(OutputFormat ExtractFormat, string prefix = "x")
+        {
+            Image gifImg = Image.FromFile(Path);
+            int frames = gifImg.GetFrameCount(FrameDimension.Time);
+
+            switch (ExtractFormat)
+            {
+                case OutputFormat.JPG:
+                    for (int i = 0; i < frames; i++)
+                    {
+                        gifImg.SelectActiveFrame(FrameDimension.Time, i);
+                        var outputFile = ExtractPath + prefix + i + ".jpg";
+                        gifImg.Save(outputFile, ImageFormat.Jpeg);
+                    }
+                    return;
+                case OutputFormat.PNG:
+                    for (int i = 0; i < frames; i++)
+                    {
+                        gifImg.SelectActiveFrame(FrameDimension.Time, i);
+                        var outputFile = ExtractPath + prefix + i + ".png";
+                        gifImg.Save(outputFile, ImageFormat.Png);
+                    }
+                    return;
+                case OutputFormat.BMP:
+                    for (int i = 0; i < frames; i++)
+                    {
+                        gifImg.SelectActiveFrame(FrameDimension.Time, i);
+                        var outputFile = ExtractPath + prefix + i + ".bmp";
+                        gifImg.Save(outputFile, ImageFormat.Bmp);
+                    }
+                    return;
+                case OutputFormat.ICO:
+                    var outputIco = ExtractPath + prefix + ".ico";
+                    gifImg.Save(outputIco, ImageFormat.Icon);
+                    return;
+            }
+        }
+    }
+}
---
+++ src/gif2png/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using MahApps.Metro.Controls;
+using System.Windows.Forms;
+using System.Collections.ObjectModel;
+
+namespace gif2png
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        private Gif gif;
+        private ObservableCollection<string> extractionFolders = new ObservableCollection<string>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            pathCombobox.ItemsSource = extractionFolders;
+            extractionFolders.Add("SELECT EXTRACTION PATH");
+            extractionFolders.Add(@"Default (\extractedFrames)");
+            extractionFolders.Add("Custom");
+        }
+
+        private void selectGIF_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog openGif = new Microsoft.Win32.OpenFileDialog
+            {
+                Filter = "GIF (*.gif)|*.gif",
+                Title = "Select GIF"
+            };
+            if (openGif.ShowDialog() == true)
+            {
+                gif = new Gif(openGif.FileName);
+
+                gifNameattrib.Text = gif.ToString();
+                gifSizeattrib.Text = $"{gif.Size().ToString()}Mb";
+                gifFramesattrib.Text = gif.FrameCount().ToString();
+            }
+        }
+
+        private void extractFrames_Click(object sender, RoutedEventArgs e)
+        {
+            var format = Gif.OutputFormat.BMP;
+            switch (formatCombobox.Text)
+            {
+                case "BMP":
+                    format = Gif.OutputFormat.BMP;
+                    break;
+                case "ICO":
+                    format = Gif.OutputFormat.ICO;
+                    break;
+                case "JPG":
+                    format = Gif.OutputFormat.JPG;
+                    break;
+                case "PNG":
+                    format = Gif.OutputFormat.PNG;
+                    break;
+                default:
+                    System.Windows.MessageBox.Show("Invalid format selected");
+                    return;
+            }
+            if (String.IsNullOrWhiteSpace(prefixInput.Text)) { gif.Extract(format, prefixInput.Text); }
+            else { gif.Extract(format, prefixInput.Text); }
+            
+        }
+
+        private void pathCombobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (pathCombobox.SelectedItem.ToString() == "Custom")
+            {
+                using (var selectextractPath = new FolderBrowserDialog())
+                {
+                    DialogResult result = selectextractPath.ShowDialog();
+
+                    if (result == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(selectextractPath.SelectedPath))
+                    {
+                        gif.SetExtractPath(selectextractPath.SelectedPath);
+                    }
+                }
+            }
+            else
+            {
+                // :/ i know.
+                if (pathCombobox.SelectedItem.ToString().Contains("Default"))
+                {
+                    System.IO.Directory.CreateDirectory(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "extractedFrames"));
+                    gif.SetExtractPath(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"extractedFrames"));
+                }
+            }
+        }
+    }
+}
